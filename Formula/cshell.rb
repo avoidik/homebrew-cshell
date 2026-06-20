b012e026175f3e46e623ec4ae7b283fd28dda38a
Viacheslav Vasilyev <avoidik@gmail.com>
2026-06-20 10:51:09 +0100

    Initial release

---
+++ Formula/cshell.rb
@@ -0,0 +1,29 @@
+class Cshell < Formula
+  desc "CLI for managing AWS CloudShell environments and opening a session"
+  homepage "https://github.com/avoidik/cshell"
+  url "https://github.com/avoidik/cshell.git", tag: "v0.1.0", revision: "c7342df73e724ad270a552860e8a5e6eceb4afae"
+  license "MIT"
+  head "https://github.com/avoidik/cshell.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+    ]
+    system "go", "build", "-trimpath", *std_go_args(ldflags: ldflags), "."
+  end
+
+  def caveats
+    <<~EOS
+      cshell hands the interactive session off to the AWS session-manager-plugin.
+      Install it separately, e.g.:
+        brew install --cask session-manager-plugin
+    EOS
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/cshell version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# homebrew-cshell
+
+Homebrew formula to install [avoidik/cshell](https://github.com/avoidik/cshell)
+
+## How to install
+
+```bash
+brew tap avoidik/cshell
+brew install cshell
+```
+
+## How to remove
+
+```bash
+brew untap avoidik/cshell
+```
